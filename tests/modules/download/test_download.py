6baaef93b97dece554a3502db3def6875d29801c
Daniel Lassahn <daniel.lassahn@gmail.com>
2020-06-24 13:00:29 +0200

    Add integration test fpr download function

--- tests/modules/download/test_download.py
+++ tests/modules/download/test_download.py
@@ -6,10 +6,11 @@ from io import BytesIO
 from pathlib import Path
 
 from src.modules.parsing.parse_grib_data import concatenate_all_variable_files
-from src.enumerations.unified_forecast_variables import ForecastVariables
 from src.enumerations.weather_models import WeatherModels
-from src.modules.config.constants import KEY_LOCAL_FILE_PATHS
-from src.modules.download.download import __download_parallel, __download
+from src.modules.config.constants import KEY_LOCAL_FILE_PATHS,\
+    KEY_REMOTE_FILE_PATHS, KEY_LOCAL_STORE_FILE_PATHS
+from src.modules.download.download import __download_parallel, __download, \
+    download
 
 
 output_file = Path(os.getcwd(), 'tests', 'modules', 'download', 'fixtures',
@@ -46,6 +47,25 @@ def test___download_parallel():
     __download_parallel([(WeatherModels.ICON_EU,
                 output_file,
                 Path('test', 'mock'))])
+
+    assert output_file.is_file() is True
+
+    os.remove(output_file)
+
+
+@patch(
+    'src.modules.download.download.urlopen',
+    MagicMock(
+        return_value=open(f"{os.getcwd()}/tests/modules/download/fixtures/"
+              f"icon-eu_europe_regular-lat-lon_single-level_2020062300_000_T_2M.grib2.bz2",
+              'rb')
+    )
+)
+def test_download():
+    download(WeatherModels.ICON_EU,
+             {KEY_LOCAL_FILE_PATHS: [output_file],
+              KEY_REMOTE_FILE_PATHS: [Path('test', 'mock')],
+              KEY_LOCAL_STORE_FILE_PATHS: [Path('not', 'used', 'in', 'download')]})
 
     assert output_file.is_file() is True
 
