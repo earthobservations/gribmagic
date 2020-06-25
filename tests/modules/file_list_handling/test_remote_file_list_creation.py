8afa9b4cbc05123535274e675d48aa9a4960cff3
Daniel Lassahn <daniel.lassahn@gmail.com>
2020-06-25 08:10:56 +0200

    Add tests reaching all parts of the file list handling code

--- tests/modules/file_list_handling/test_remote_file_list_creation.py
+++ tests/modules/file_list_handling/test_remote_file_list_creation.py
@@ -1,13 +1,20 @@
-from src.modules.file_list_handling.remote_file_list_creation import \
-    build_remote_file_lists_for_variable_files, build_remote_file_lists_for_package_files
-from src.enumerations.weather_models import WeatherModels
 from datetime import datetime
 from pathlib import Path
-import pytest
-from src.exceptions.wrong_weather_model_exception import WrongWeatherModelException
 from unittest.mock import patch
-from src.modules.config.constants import KEY_VARIABLES, KEY_GRIB_PACKAGE_TYPES, KEY_FORECAST_STEPS, \
-    KEY_DIRECTORY_TEMPLATE, KEY_REMOTE_SERVER, KEY_FILE_TEMPLATE, KEY_INITIALIZATION_DATE_FORMAT
+
+import pytest
+
+from src.enumerations.weather_models import WeatherModels
+from src.exceptions.wrong_weather_model_exception import \
+    WrongWeatherModelException
+from src.modules.config.constants import KEY_VARIABLES, KEY_GRIB_PACKAGE_TYPES, \
+    KEY_FORECAST_STEPS, \
+    KEY_DIRECTORY_TEMPLATE, KEY_REMOTE_SERVER, KEY_FILE_TEMPLATE, \
+    KEY_INITIALIZATION_DATE_FORMAT
+from src.modules.file_list_handling.remote_file_list_creation import \
+    build_remote_file_lists_for_variable_files, \
+    build_remote_file_lists_for_package_files, \
+    build_remote_file_list
 
 
 @patch(
@@ -26,18 +33,23 @@ from src.modules.config.constants import KEY_VARIABLES, KEY_GRIB_PACKAGE_TYPES, 
 )
 def test_build_remote_model_file_lists():
     to_test = build_remote_file_lists_for_variable_files(WeatherModels.ICON_EU,
-                           0,
-                           datetime(2020, 6, 10).date())
-    assert to_test == [Path('test1/test_remote_dir/00/t_2m/test_remote_file_single-level_2020061000_000_T_2M.grib2.bz2'),
-                       Path('test1/test_remote_dir/00/t_2m/test_remote_file_single-level_2020061000_001_T_2M.grib2.bz2')]
+                                                         0,
+                                                         datetime(2020, 6,
+                                                                  10).date())
+    assert to_test == [Path(
+        'test1/test_remote_dir/00/t_2m/test_remote_file_single-level_2020061000_000_T_2M.grib2.bz2'),
+                       Path(
+                           'test1/test_remote_dir/00/t_2m/test_remote_file_single-level_2020061000_001_T_2M.grib2.bz2')]
 
 
 def test_build_remote_model_file_lists_wrong_weather_model():
     with pytest.raises(WrongWeatherModelException) as exc:
-        _ = build_remote_file_lists_for_variable_files(WeatherModels.AROME_METEO_FRANCE,
-                                                             0,
-                                                             datetime(2020, 6, 10).date())
-    assert str(exc.value) == 'Please choose one of [icon_global, icon_eu, cosmo_d2]'
+        _ = build_remote_file_lists_for_variable_files(
+            WeatherModels.AROME_METEO_FRANCE,
+            0,
+            datetime(2020, 6, 10).date())
+    assert str(
+        exc.value) == 'Please choose one of [icon_global, icon_eu, cosmo_d2]'
 
 
 @patch(
@@ -55,8 +67,42 @@ def test_build_remote_model_file_lists_wrong_weather_model():
             }}
 )
 def test_build_remote_model_file_lists_for_package():
-    to_test = build_remote_file_lists_for_package_files(WeatherModels.AROME_METEO_FRANCE,
-                           0,
-                           datetime(2020, 6, 10).date())
-    assert to_test == [Path('test1/test_remote_file_2020061000_00_Package1.grib2.bz2'),
-                       Path('test1/test_remote_file_2020061000_01_Package1.grib2.bz2')]
+    to_test = build_remote_file_lists_for_package_files(
+        WeatherModels.AROME_METEO_FRANCE,
+        0,
+        datetime(2020, 6, 10).date())
+    assert to_test == [
+        Path('test1/test_remote_file_2020061000_00_Package1.grib2.bz2'),
+        Path('test1/test_remote_file_2020061000_01_Package1.grib2.bz2')]
+
+@patch(
+    'src.modules.file_list_handling.remote_file_list_creation.MODEL_CONFIG',
+    {
+        WeatherModels.AROME_METEO_FRANCE.value:
+            {
+                KEY_VARIABLES: ['air_temperature_2m'],
+                KEY_FORECAST_STEPS: {0: [0, 1]},
+                KEY_FILE_TEMPLATE: 'test_remote_file_{initialization_date}{initialization_time}_'
+                                   '{forecast_step}_{grib_package_type}.grib2.bz2',
+                KEY_REMOTE_SERVER: 'test1',
+                KEY_INITIALIZATION_DATE_FORMAT: '%Y%m%d',
+                KEY_GRIB_PACKAGE_TYPES: ['Package1']
+            }}
+)
+def test_build_remote_file_list():
+    to_test = build_remote_file_list(
+        WeatherModels.AROME_METEO_FRANCE,
+        0,
+        datetime(2020, 6, 10).date())
+    assert to_test == [
+        Path('test1/test_remote_file_2020061000_00_Package1.grib2.bz2'),
+        Path('test1/test_remote_file_2020061000_01_Package1.grib2.bz2')]
+
+
+def test_build_remote_model_file_lists_for_package_wrong_model():
+    with pytest.raises(WrongWeatherModelException) as excinfo:
+        _ = build_remote_file_lists_for_package_files(WeatherModels.ICON_EU,
+                                                      0,
+                                                      datetime(2020, 6,
+                                                               10).date())
+    assert str(excinfo.value) == 'Please choose one of [arome_meteo_france, geos5]'
