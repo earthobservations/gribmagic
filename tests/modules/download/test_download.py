45a6299e8d7e9d6b675e8780dbdf34b68950d5ce
Daniel Lassahn <daniel.lassahn@gmail.com>
2020-06-25 08:05:07 +0200

    Add tests reaching all parts of the download code

--- tests/modules/download/test_download.py
+++ tests/modules/download/test_download.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 from src.enumerations.weather_models import WeatherModels
 from src.modules.config.constants import KEY_LOCAL_FILE_PATHS,\
-    KEY_REMOTE_FILE_PATHS, KEY_LOCAL_STORE_FILE_PATHS
+    KEY_REMOTE_FILE_PATHS, KEY_LOCAL_STORE_FILE_PATHS, KEY_COMPRESSION
 from src.modules.download.download import __download_parallel, __download, \
     download
+from src.modules.config.configurations import MODEL_CONFIG as test_config
 
+test_config[WeatherModels.ICON_EU.value][KEY_COMPRESSION] = ''
 
 output_file = Path(os.getcwd(), 'tests', 'modules', 'download', 'fixtures',
                    'air_temperature_2m.grib2')
@@ -64,7 +66,6 @@ def test_download_bunzip():
               KEY_LOCAL_STORE_FILE_PATHS: [Path('not', 'used', 'in', 'download')]})
 
     assert output_file.is_file() is True
-
     os.remove(output_file)
 
 
@@ -76,11 +77,36 @@ def test_download_bunzip():
               'rb')
     )
 )
+@patch(
+    'src.modules.download.download.MODEL_CONFIG',
+    MagicMock(
+        return_value=test_config
+    )
+)
 def test_download_store():
     download(WeatherModels.ICON_EU,
              {KEY_LOCAL_FILE_PATHS: [output_file],
               KEY_REMOTE_FILE_PATHS: [Path('test', 'mock')],
               KEY_LOCAL_STORE_FILE_PATHS: [Path('not', 'used', 'in', 'download')]})
+
+    assert output_file.is_file() is True
+    os.remove(output_file)
+
+
+@patch(
+    'src.modules.download.download.urlopen',
+    MagicMock(
+        return_value=open(f"{os.getcwd()}/tests/modules/download/fixtures/"
+              f"icon-eu_europe_regular-lat-lon_single-level_2020062300_000_T_2M.grib2.bz2",
+              'rb')
+    )
+)
+def test_download_parallel():
+    download(WeatherModels.ICON_EU,
+             {KEY_LOCAL_FILE_PATHS: [output_file],
+              KEY_REMOTE_FILE_PATHS: [Path('test', 'mock')],
+              KEY_LOCAL_STORE_FILE_PATHS: [Path('not', 'used', 'in', 'download')]},
+             parallel_download=True)
 
     assert output_file.is_file() is True
 
