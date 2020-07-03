1761f5704d336a068d371dbdc0c75ff118791a92
Daniel Lassahn <daniel.lassahn@gmail.com>
2020-07-03 13:56:14 +0200

    Add first thoughts about harmonie arome

--- src/enumerations/weather_models.py
+++ src/enumerations/weather_models.py
@@ -5,9 +5,12 @@ from enum import Enum
 class WeatherModels(Enum):
     """Enumeration of weather models"""
     ICON_EU = 'icon_eu'
+    ICON_EU_EPS = 'icon_eu_eps'
     ICON_GLOBAL = 'icon_global'
     COSMO_D2 = 'cosmo_d2'
+    COSMO_D2_EPS = 'cosmo_d2'
     GFS_025 = 'ncep_gfs_025'
     AROME_METEO_FRANCE = 'arome_meteo_france'
+    HARMONIE_KNMI = 'harmonie_knmi'
     ARPEGE = 'arpege'
     GEOS5 = 'geos_5'
