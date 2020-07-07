72b355345a07812e982a3b57561d3979c8cd47a2
Daniel Lassahn <daniel.lassahn@gmail.com>
2020-07-07 14:43:09 +0200

    Keys_from data

--- src/modules/parsing/parse_grib_data.py
+++ src/modules/parsing/parse_grib_data.py
@@ -22,3 +22,22 @@ def concatenate_all_variable_files(
     variable_file_list = \
         [file for file in model_file_list[KEY_LOCAL_FILE_PATHS] if variable.value in str(file)]
     return xarray.open_mfdataset(variable_file_list, engine='cfgrib')
+
+# import cfgrib
+#
+# data = cfgrib.open_datasets('')
+#
+# variables_inventory = {}
+# for idx, dat in enumerate(data):
+#     for item in dat.variables.items():
+#         if item[0] not in ['time', 'latitude', 'longitude', 'valid_time', 'atmosphere', 'step', 'heightAboveGround','isobaricInPa', 'isobaricInPa', 'heightAboveSea', 'hybrid', 'isothermZero',  'potentialVorticity', 'nominalTop', 'sigmaLayer','surface',   'pressureFromGroundLayer',  'tropopause','level',   'maxWind',   'sigma']:
+#             try:
+#                 inventory_info = {'level_type': dat[item[0]].attrs['GRIB_typeOfLevel'],
+#                                                     'list_index': idx}
+#             except KeyError:
+#                 inventory_info = {'level_type': '',
+#                                   'list_index': idx}
+#             if item[0] in variables_inventory.keys():
+#                 variables_inventory[item[0]].append(inventory_info)
+#             else:
+#                 variables_inventory[item[0]] = [inventory_info]
